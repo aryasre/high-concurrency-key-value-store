8a06cbb179d97fbb24fdee5b83e92fda34f2ffbd
Arya Sreekumar <aryasree2204@gmail.com>
2025-11-06 15:40:34 +0530

    Create wal log for crash safe persistent storage

--- kv_store.c
+++ kv_store.c
@@ -28,6 +28,7 @@ void put(const char *key, const char *value) {
     strcpy(newNode->value, value);
     newNode->next = hash_table[index];
     hash_table[index] = newNode;
+    atomic_fetch_add(&write_count, 1);
     pthread_rwlock_unlock(&rw_lock);
 }
 
@@ -49,6 +50,50 @@ char *get(const char *key) {
     return NULL;
 }
 
+/* ---------- WAL Append ---------- */
+void wal_append(const char *key, const char *value) {
+    pthread_mutex_lock(&wal_lock);
+    int fd = open(WAL_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd < 0) {
+        perror("open WAL failed");
+        pthread_mutex_unlock(&wal_lock);
+        return;
+    }
+
+    char buffer[KEY_SIZE + VALUE_SIZE + 8];
+    snprintf(buffer, sizeof(buffer), "%s %s\n", key, value);
+    write(fd, buffer, strlen(buffer));
+    fsync(fd);
+    close(fd);
+
+    pthread_mutex_unlock(&wal_lock);
+}
+
+/* ---------- WAL Replay ---------- */
+void wal_replay() {
+    int fd = open(WAL_FILE, O_RDONLY);
+    if (fd < 0) {
+        printf("[Recovery] No WAL found. Starting fresh......\n");
+        return;
+    }
+
+    FILE *fp = fdopen(fd, "r");
+    if (!fp) {
+        perror("fdopen");
+        close(fd);
+        return;
+    }
+
+    char key[KEY_SIZE], value[VALUE_SIZE];
+    while (fscanf(fp, "%s %s", key, value) == 2) 
+    {
+     put(key, value); 
+    }
+    fclose(fp);
+    printf("[Recovery] WAL replay complete.\n");
+    sleep(2);
+}
+
 
 /*---------- Writer Thread function ----------*/
 void *writer_thread(void *arg) {
@@ -60,6 +105,7 @@ void *writer_thread(void *arg) {
         snprintf(key, sizeof(key), "key %d_%d", id, i);
         int res_value = (id * 1000) + i;
         snprintf(value, sizeof(value), "%d", res_value);
+        wal_append(key, value);
         put(key, value);
 
         printf("[Writer %d] Wrote [%s : %s]\n", id, key, value);
@@ -110,7 +156,7 @@ void display_table() {
 int main() {
 
     printf("----------Key-Value Store--------------------\n");
-
+    wal_replay();
     pthread_t writers[MAX_WRITER], readers[MAX_READER]; 
     int wid[MAX_WRITER] = {1, 2, 3}, rid[MAX_READER] = {1, 2};
 
@@ -125,5 +171,6 @@ int main() {
         pthread_join(readers[i], NULL);
 
     display_table(); 
+    printf("\nTotal successful writes: %d\n", atomic_load(&write_count));
     return 0;
 }
--- kv_store.h
+++ kv_store.h
@@ -17,6 +17,7 @@
 #define BUF_SIZE   128
 #define MAX_WRITER  3
 #define MAX_READER  2 
+#define WAL_FILE   "wal.log"
 
 typedef struct Node {
     char key[KEY_SIZE];
@@ -28,10 +29,15 @@ Node *hash_table[TABLE_SIZE];
 
 /* ---------- Initialising Locks ----------*/
 pthread_rwlock_t rw_lock = PTHREAD_RWLOCK_INITIALIZER;
+pthread_mutex_t wal_lock = PTHREAD_MUTEX_INITIALIZER;
+
+atomic_int write_count = 0;
 
 unsigned int hash(const char *key);
 void put(const char *key, const char *value);
 char *get(const char *key);
+void wal_append(const char *key, const char *value);
+void wal_replay();
 void *writer_thread(void *arg);
 void *reader_thread(void *arg);
 void display_table();
